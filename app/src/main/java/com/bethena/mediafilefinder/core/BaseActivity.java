20a92f1ab48f9e94085e68b0730b0f9039e8d3ec
chenminglin <806663326@qq.com>
2018-09-09 16:08:19 +0800

    尝试使用AsyncTask调用。

--- app/src/main/java/com/bethena/mediafilefinder/core/BaseActivity.java
+++ app/src/main/java/com/bethena/mediafilefinder/core/BaseActivity.java
@@ -16,6 +16,8 @@ import com.bethena.mediafilefinder.R;
 
 public class BaseActivity extends AppCompatActivity {
 
+    protected final String TAG = getClass().getSimpleName();
+
     String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
